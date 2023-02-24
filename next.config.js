d0d8e63a8398b1af5d4cb45973f46bb7f604db8e
Nikhil M <nrmeria@gmail.com>
2023-02-24 19:38:14 +0530

    pwa now working

---
+++ next.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
+
+const plugins = []
+
+if (process.env.ANALYZE === 'true') {
+  // only load dependency if env `ANALYZE` was set
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true,
+  })
+
+  plugins.push(withBundleAnalyzer)
+}
+
+plugins.push(withPWA)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new CircularDependencyPlugin({
+        exclude: /a\.js|node_modules/,
+        include: /src/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      }),
+    )
+
+    return config
+  },
+}
+
+module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig)
+
