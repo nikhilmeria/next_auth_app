45e49b9c910f954a51fe64c092705a1cc31ae6a8
Nikhil M <nrmeria@gmail.com>
2023-03-23 12:17:26 +0530

    storing data in serv collection in db- DONE

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,9 +1,30 @@
-export default function formHandler(req, res) {
+import firebase_app from "../../firebase_config";
+import {getFirestore, collection, addDoc} from "firebase/firestore";
+
+const db = getFirestore(firebase_app);
+
+export default async function formHandler(req, res) {
+ const name = req.body.name;
+ const no = req.body.no;
+ const serv = req.body.serv;
+ const address = req.body.address;
+
  if (req.method === "POST") {
-  console.log("body1: ", req.body.name);
-  console.log("body2: ", req.body.no);
-  console.log("body3: ", req.body.serv);
-  console.log("body3: ", req.body.address);
+  try {
+   const docRef = await addDoc(collection(db, "services"), {
+    name: name,
+    phone_no: no,
+    service_req: serv,
+    address: address,
+   });
+   console.log("Document written with ID: ", docRef.id);
+  } catch (err) {
+   console.error("err in form api = ", err);
+  }
+  //   console.log("body1: ", req.body.name);
+  //   console.log("body2: ", req.body.no);
+  //   console.log("body3: ", req.body.serv);
+  //   console.log("body3: ", req.body.address);
 
   res.status(201).json({data: "Form processing by api"});
  }
