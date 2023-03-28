093d23580692f8c92325376f72b6c50f04d2494b
Nikhil M <nrmeria@gmail.com>
2023-03-28 13:06:46 +0530

    still working on about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,21 +5,31 @@ import aboutStyles from "../../styles/About.module.css";
 function About() {
  return (
   <div className={aboutStyles.main}>
-   <h3>About</h3>
+   <header>
+    <nav className={aboutStyles.navG}>
+     <Image
+      className={aboutStyles.navGImg}
+      src="/images/logo.png"
+      alt="co logo"
+      width={50}
+      height={48}
+     />
+    </nav>
+   </header>
 
    <div className={aboutStyles.img}>
     <Image
-     src="/images/about.jpg"
+     src="/images/about.webp"
      layout="fill"
      objectFit="cover"
      alt="image"
     ></Image>
    </div>
 
-   <div className={aboutStyles.section}>
+   {/* <div className={aboutStyles.section}>
     <div className={aboutStyles.secChild}>
      <Image></Image>
-     <h4></h4>
+     <h4>Services</h4>
      <p></p>
     </div>
     <div className={aboutStyles.secChild}>
@@ -37,7 +47,7 @@ function About() {
      <h4></h4>
      <p></p>
     </div>
-   </div>
+   </div> */}
 
    <div className={aboutStyles.stmt}>
     <h2>A Mission to Make an Impact</h2>
@@ -51,21 +61,64 @@ function About() {
    </div>
 
    <div className={aboutStyles.card}>
-    <div className={aboutStyles.cardImg}>
-     <Image
-      src="/images/commit.png"
-      alt={title}
-      width={150}
-      height={150}
-      objectFit="cover"
-     />
+    <div className={aboutStyles.cardBox}>
+     <div className={aboutStyles.cardImg}>
+      <Image
+       src="/images/co.webp"
+       alt="cardImg"
+       width={150}
+       height={150}
+       objectFit="cover"
+      />
+     </div>
+     <div className={aboutStyles.cardContent}>
+      <h2>Company</h2>
+      <p>
+       Capital One was founded on the belief that no one should be locked out of
+       the financial system. Today, our deep commitment to financial inclusion
+       is
+      </p>
+     </div>
     </div>
-    <div className={aboutStyles.cardContent}>
-     <h2>Card</h2>
-     <p>
-      Capital One was founded on the belief that no one should be locked out of
-      the financial system. Today, our deep commitment to financial inclusion is
-     </p>
+
+    <div className={aboutStyles.cardBox}>
+     <div className={aboutStyles.cardImg}>
+      <Image
+       src="/images/carrer.webp"
+       alt="cardImg"
+       width={150}
+       height={150}
+       objectFit="cover"
+      />
+     </div>
+     <div className={aboutStyles.cardContent}>
+      <h2>Carrer</h2>
+      <p>
+       Capital One was founded on the belief that no one should be locked out of
+       the financial system. Today, our deep commitment to financial inclusion
+       is
+      </p>
+     </div>
+    </div>
+
+    <div className={aboutStyles.cardBox}>
+     <div className={aboutStyles.cardImg}>
+      <Image
+       src="/images/commit.png"
+       alt="cardImg"
+       width={150}
+       height={150}
+       objectFit="cover"
+      />
+     </div>
+     <div className={aboutStyles.cardContent}>
+      <h2>Services</h2>
+      <p>
+       Capital One was founded on the belief that no one should be locked out of
+       the financial system. Today, our deep commitment to financial inclusion
+       is
+      </p>
+     </div>
     </div>
    </div>
   </div>
