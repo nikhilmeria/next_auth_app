2a4c751bd4125e9cb6f1baa8a5e7a9288bf5d2a8
Nikhil M <nrmeria@gmail.com>
2023-04-15 16:55:44 +0530

    updated - firestore query by date

--- pages/admin/services.js
+++ pages/admin/services.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import adminServStyles from "../../styles/Adm_serv.module.css";
 
 import firebase_app from "../../firebase_config";
@@ -11,46 +11,35 @@ import {
  getDocs,
  onSnapshot,
  limit,
- Timestamp,
 } from "@firebase/firestore";
 
 const db = getFirestore(firebase_app);
 
 function Services() {
  const [selectedDate, setSelectedDate] = useState("");
+ const [data, setData] = useState([]);
+
+ useEffect(() => {
+  console.log("Data has been updated: ", data);
+ }, [data]);
 
  const queryServices = async () => {
-  const dt = new Date(selectedDate);
-  const myStamp = Timestamp.fromDate(dt);
   console.log("selectedDate -> ", selectedDate);
-  console.log("dt 1 -> ", dt);
-  console.log("dt 2 -> ", dt.toLocaleDateString());
-  console.log("myStamp -> ", myStamp.toDate());
 
-  const querySnapshot = await getDocs(
-   query(
-    collection(db, "users"),
-    where("date", ">=", new Date("2023-04-10")),
-    where("date", "<=", new Date(selectedDate))
-   )
-  );
+  try {
+   const querySnapshot = await getDocs(
+    query(
+     collection(db, "services"),
+     where("date", ">=", new Date("2023-04-10")),
+     where("date", "<=", new Date(selectedDate))
+    )
+   );
 
-  const filteredData = querySnapshot.docs.map((doc) => {
-   const data = doc.data();
-   console.log("Records = ", data);
-  });
-
-  //   const q = query(
-  //    collection(db, "services"),
-  //    orderBy("address", "asc"),
-  //    limit(10)
-  //   );
-  //   onSnapshot(q, (snapshot) => {
-  //    const Records = snapshot.docs.map((doc) => ({
-  //     dtd: doc.data().date.toDate(),
-  //    }));
-  //    console.log("Records ", Records);
-  //   });
+   const tempData = querySnapshot.docs.map((doc) => doc.data());
+   setData(tempData);
+  } catch (error) {
+   console.error("Error getting documents: ", error);
+  }
  };
 
  return (
@@ -67,10 +56,3 @@ function Services() {
 }
 
 export default Services;
-
-// console.log("doc : ", doc.data().date.toDate().toLocaleDateString());
-// let dd = new Date(doc.data().date);
-// console.log("dd : ", dt.toLocaleDateString());
-// if (doc.data().date.toDate().toLocaleDateString() === dt.toLocaleDateString()) {
-//  console.log("Done....");
-// }
