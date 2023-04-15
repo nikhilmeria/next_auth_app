28924314bed14d2979ccd8da49d699707dd36a7c
Nikhil M <nrmeria@gmail.com>
2023-04-15 20:52:52 +0530

    admin serv page styled

--- pages/admin/services.js
+++ pages/admin/services.js
@@ -16,7 +16,8 @@ import {
 const db = getFirestore(firebase_app);
 
 function Services() {
- const [selectedDate, setSelectedDate] = useState("");
+ const [startDate, setStartDate] = useState("");
+ const [endDate, setEndDate] = useState("");
  const [data, setData] = useState([]);
 
  useEffect(() => {
@@ -24,14 +25,15 @@ function Services() {
  }, [data]);
 
  const queryServices = async () => {
-  console.log("selectedDate -> ", selectedDate);
+  console.log("endDate -> ", endDate);
+  console.log("startDate -> ", startDate);
 
   try {
    const querySnapshot = await getDocs(
     query(
      collection(db, "services"),
-     where("date", ">=", new Date("2023-04-10")),
-     where("date", "<=", new Date(selectedDate))
+     where("date", ">=", new Date(startDate)),
+     where("date", "<=", new Date(endDate))
     )
    );
 
@@ -47,10 +49,31 @@ function Services() {
    <h3>Admin - Services</h3>
    <input
     type="date"
-    value={selectedDate}
-    onChange={(e) => setSelectedDate(e.target.value)}
+    value={startDate}
+    placeholder="Start Date"
+    onChange={(e) => setStartDate(e.target.value)}
+   />
+   <input
+    type="date"
+    value={endDate}
+    placeholder="End Date"
+    onChange={(e) => setEndDate(e.target.value)}
    />
    <button onClick={queryServices}>Search</button>
+   <div className={adminServStyles.result}>
+    {data.length === 0 ? (
+     <h4>No Data Found</h4>
+    ) : (
+     <>
+      {data.map((ei) => (
+       <div key={ei.date}>
+        <h2>{ei.name}</h2>
+        <p>{ei.address}</p>
+       </div>
+      ))}
+     </>
+    )}
+   </div>
   </div>
  );
 }
